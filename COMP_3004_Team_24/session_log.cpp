7a1b55d5d77b48e30540a3ec0c6ee8aae4e2ba93
Kiran Adhikari <69723199+kiranadhikari1@users.noreply.github.com>
2024-04-02 11:20:03 -0400

    session_log class & header cleanup
    fixed session_log .h and .cpp files

--- COMP_3004_Team_24/session_log.cpp
+++ COMP_3004_Team_24/session_log.cpp
@@ -1,16 +1,5 @@
 #include "session_log.h"
 
-class SessionLog {
-public:
-    // Adds a new session to the log
-    void addSession(const SessionData& data);
+void SessionLog:: exportData() const{
 
-    // Retrieves session history
-    std::vector<SessionData> getSessionHistory() const;
-
-    // Exports session data to a PC
-    void exportData() const;
-
-private:
-    std::vector<SessionData> sessionHistory;
-};
+}
---
+++ COMP_3004_Team_24/session_log.h
@@ -0,0 +1,15 @@
+#ifndef SESSION_LOG_H
+#define SESSION_LOG_H
+
+class SessionLog {
+
+public:
+    void exportData() const;
+    // void addSession(const SessionData& data);
+    // std::vector<SessionData> getSessionHistory() const;
+private:
+    // std::vector<SessionData> sessionHistory;
+};
+
+
+#endif // SESSION_LOG_H
